6e0eae9499dd3ef440b86f4ee26d71a055eb20fb
Jinyang <jinyangustc@gmail.com>
2015-04-21 20:37:37 +0800

    include hammerspoon, oh-my-zsh, ec dotfiles

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ~
+ln -s ~/dotfiles/latexmkrc ~/.latexmkrc
+ln -s ~/dotfiles/hammerspoon ~/.hammerspoon
+ln -s ~/dotfiles/oh-my-zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/oh-my-zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/ec /usr/local/bin/ec
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ~
+rm ~/.latexmkrc
+rm ~/.hammerspoon
+rm ~/.oh-my-zsh
+rm ~/.zshrc
+rm /usr/local/bin/ec
