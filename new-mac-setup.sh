8e6cb161c1d439359c0e287073b5399a9a4f0337
jimyl <jinyangustc@gmail.com>
2015-04-28 01:22:03 +0800

    add new mac setup script

---
+++ new-mac-setup.sh
@@ -0,0 +1,30 @@
+cd ~
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+\curl -sSL https://get.rvm.io | bash
+
+
+brew install git
+brew install caskroom/cask/brew-cask
+brew update
+brew cask update
+
+brew install --with-srgb --with-cocoa emacs
+brew install wget
+brew install tree
+brew install htop
+
+brew cask install iterm2
+brew cask install alfred
+brew cask install tunnelblick
+brew cask install hammerspoon
+brew cask install firefox
+brew cask install pycharm
+brew cask install skype
+brew cask install dropbox
+brew cask install racket
+brew cask install qq
+brew cask install caffeine
+brew cask install flux
+brew cask install mactex
